48a0dca3da5292a03185485013f79acc59306e77
Universe <tmdwnchlrh3@gmail.com>
2021-10-01 16:47:54 +0900

    DDL 추가

--- scott.sql
+++ scott.sql
@@ -2037,9 +2037,85 @@ UPDATE DEPT_TCL
 SET LOC = 'SEOUL'
 WHERE DEPTNO = 30;
 COMMIT;
+select * from dept_temp;
+
+-- DDL (데이터 정의어)
+-- 객체를 생성, 변경, 삭제
+-- 실행하면 반영됨 
+
+-- CREATE(생성), ALTER(변경), DROP(삭제)
+-- CREATE TABLE 테이블 이름(
+--          필드명1 자료형,
+--          필드명2 자료형, ...
+--       )
+
+-- 테이블 이름 만드는 규칠 
+-- 문자로 시작
+-- 같은 사용자 안에서 동일한 테이블 명 사용불가
+-- SQL 키워드는 테이블 이름으로 할 수 없음
+
+-- 필드명 생성 규칙
+-- 문자로 시작
+-- 열 이름은 중복되면 안됨
+-- SQL 키워드 사용불가 
+
+-- 자료형 : 숫자 NUMBER, 문자 VARCHAR2, 날짜 DATE
+CREATE TABLE EMP_DDL(
+    EMPNO NUMBER(4), -- 숫자로 4자리까지만 허용하겠음
+    ENAME VARCHAR2(10),
+    JOB VARCHAR2(9),
+    MGR NUMBER(4),
+    HIREDATE DATE,
+    SAL NUMBER(7,2), -- 숫자로 7자리까지 허용, 소수점 2자리 허용
+    COMM NUMBER(7,2),
+    DEPTNO NUMBER(2)
+    );
+DROP TABLE EMP_DDL;
+-- 구조 + 데이터 이용하여 생성
+CREATE TABLE DEPT_DDL AS SELECT * FROM DEPT;
+CREATE TABLE DEPT_DDL_30 AS SELECT * FROM DEPT WHERE deptno= 30;
+
+-- 다른 테이블 구조만 복사하여 새 테이블 생성
+CREATE TABLE DEPT_DDL2 AS SELECT * FROM DEPT WHERE 1<>1;
+
+-- ALTER
+-- 이미 생성된 객체를 변경
+CREATE TABLE EMP_ALTER AS SELECT * FROM EMP;
 
 
+-- 새로운 컬럼(열) 추가 
+ALTER  TABLE EMP_ALTER ADD HP VARCHAR2(20);
+DESC EMP_ALTER;
+SELECT * FROM EMP_ALTER;
+-- RENAME : 열이름 변경
+ALTER TABLE EMP_ALTER RENAME COLUMN HP TO TEL;
+-- MODIFY : 열의 자료형을 변경
+ALTER TABLE EMP_ALTER MODIFY EMPNO NUMBER(5);
+-- DROP : 열 삭제 
+ALTER TABLE EMP_ALTER DROP COLUMN TEL;
 
+-- 테이블 이름 변경
+RENAME EMP_ALTER TO EMP_RENAME;
+SELECT * FROM EMP_RENAME;
+
+-- 테이블 데이터를 모두 삭제  TRUNCATE
+TRUNCATE TABLE EMP_RENAME;
+SELECT * FROM EMP_RENAME;
+
+-- VARCHAR2 가변문자
+CREATE TABLE member (
+        id char(8),
+        name varchar2(10),
+        addr varchar2(50),
+        nation char(4),
+        email  varchar2(50),
+        age NUMBER(7)
+        );
+ALTER TABLE MEMBER ADD BINGO VARCHAR2(20); 
+ALTER TABLE MEMBER MODIFY BINGO VARCHAR2(30);
+ALTER TABLE MEMBER RENAME COLUMN BINGO TO REMARK;
+SELECT * FROM MEMBER;
+        
 
 
 
